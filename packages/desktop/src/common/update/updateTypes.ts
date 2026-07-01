01b3b767129c6d0a9b62b4763fef5b3275921408
VNDT1625 <vndt1625@gmail.com>
2026-07-01 08:45:35 +0700

    fix(release): point updater to tomny repo

--- packages/desktop/src/common/update/updateTypes.ts
+++ packages/desktop/src/common/update/updateTypes.ts
@@ -35,7 +35,7 @@ export interface UpdateCheckResult {
 
 export interface UpdateCheckRequest {
   includePrerelease?: boolean;
-  /** Defaults to iOfficeAI/AionUi when omitted */
+  /** Defaults to VNDT1625/OmniAgent when omitted */
   repo?: string;
 }
 
